64138c5f5a062f9201d68563f55f414525fe301e
Streetmeat25 <kotpersikchannel@gmail.com>
2025-03-11 20:32:48 +0300

    task2 optimisation

--- 11.03.2025/task02.py
+++ 11.03.2025/task02.py
@@ -9,20 +9,26 @@ We guarantee, that the given sequence contain >= 0 integers inside.
 from collections.abc import Sequence
 
 
-def check_fibonacci(data: Sequence[int]) -> bool:
-    if data[0] == 0 and data[1] == 1:
+# def check_fibonacci(data: Sequence[int]) -> bool:
+#     if data[0] == 0 and data[1] == 1:
 
-        for index, item in enumerate(data, start=0):
+#         for index, item in enumerate(data, start=0):
             
-            if index == 0:
-                continue
-            if index == 1:
-                continue
+#             if index == 0:
+#                 continue
+#             if index == 1:
+#                 continue
             
-            if data[index] != data[index-1] + data[index-2]:
-                return False
-            else:
-                return True    
+#             if data[index] != data[index-1] + data[index-2]:
+#                 return False
+#             else:
+#                 return True    
+#     return False
+
+def check_fibonacci(data: Sequence[int]) -> bool:
+    if data[0] == 0 and data[1] == 1 and len(data) > 2:
+        for i in range(2, len(data)):
+            return data[i] == data[i-1] + data[i-2]
     return False
 
 print(check_fibonacci([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]))
