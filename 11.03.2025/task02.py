edc6868dfe5d6ad0bb435c77f37447d638438795
Streetmeat25 <kotpersikchannel@gmail.com>
2025-03-11 19:34:29 +0300

    task02 done, python syntax fighting continues

--- 11.03.2025/task02.py
+++ 11.03.2025/task02.py
@@ -6,8 +6,26 @@ Given a cell with "it's a fib sequence" from slideshow,
 We guarantee, that the given sequence contain >= 0 integers inside.
 
 """
-from collections import Sequence
+from collections.abc import Sequence
 
 
 def check_fibonacci(data: Sequence[int]) -> bool:
-    ...
+    if data[0] == 0 and data[1] == 1:
+
+        for index, item in enumerate(data, start=0):
+            
+            if index == 0:
+                continue
+            if index == 1:
+                continue
+            
+            if data[index] != data[index-1] + data[index-2]:
+                print('false')
+                return False
+            else:
+                print('true') 
+                return True
+    print('false')     
+    return False
+
+check_fibonacci([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89])
